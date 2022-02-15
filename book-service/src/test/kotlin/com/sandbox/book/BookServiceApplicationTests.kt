e44b27b7f8449774bb1c0e4fb30b6af7a2c8daf4
johannfazakas <johannfazakas@gmail.com>
2022-02-15 17:20:45 +0200

    simple spring book service

---
+++ book-service/src/test/kotlin/com/sandbox/book/BookServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sandbox.book
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
