ce4e6ed2ed0a160776c0533dd2ef6e6d8eb370d8
johannfazakas <johannfazakas@gmail.com>
2022-02-21 10:15:46 +0200

    integrated jooq domain generation, introduced questionable build time dependency to running db

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 ### Docker
 ```bash
 cd book-service
+# jar creation separated from image build due to jooq's build time dependency to running db
+./gradlew clean bootJar
 docker build -t book-service .
 
 docker network create --driver bridge to-read-network
@@ -14,6 +16,11 @@ docker run -p 8015:8080 --net=to-read-network -d book-service
 
 ### Docker compose
 ```bash
+# jar creation separated from image build due to jooq's build time dependency to running db.
+cd book-service
+./gradlew clean bootJar
+cd ../
+
 docker compose build
 docker compose up
 ```
--- book-service/build.gradle.kts
+++ book-service/build.gradle.kts
@@ -1,6 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+buildscript {
+	configurations["classpath"].resolutionStrategy.eachDependency {
+		if (requested.group == "org.jooq") {
+			useVersion("3.16.1")
+		}
+	}
+}
+
+
 plugins {
 	id("org.springframework.boot") version "2.6.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
@@ -8,6 +17,7 @@ plugins {
 	kotlin("plugin.spring") version "1.6.10"
 	kotlin("plugin.jpa") version "1.6.10"
 	id("org.jetbrains.kotlin.plugin.noarg") version "1.6.10"
+	id("nu.studer.jooq") version "7.1.1"
 }
 
 group = "com.tr"
@@ -19,11 +29,21 @@ repositories {
 }
 
 dependencies {
+	// spring
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	// kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	// postgresql
 	runtimeOnly("org.postgresql:postgresql")
+	// jooq
+	compileOnly("org.jooq:jooq:3.16.4")
+	compileOnly("org.jooq:jooq-meta:3.16.3")
+	compileOnly("org.jooq:jooq-codegen:3.16.3")
+	jooqGenerator("jakarta.xml.bind:jakarta.xml.bind-api:3.0.1")
+	jooqGenerator("org.postgresql:postgresql:42.3.2")
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -45,4 +65,43 @@ tasks.getByName<BootJar>("bootJar") {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jooq {
+	version.set("3.16.4")
+	edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
+
+	configurations {
+		create("main") {
+			generateSchemaSourceOnCompilation.set(true)
+
+			jooqConfiguration.apply {
+				jdbc.apply {
+					driver = "org.postgresql.Driver"
+					url = "jdbc:postgresql://localhost:5432/books"
+					user = "postgres"
+					password = "admin"
+				}
+
+				generator.apply {
+					name = "org.jooq.codegen.KotlinGenerator"
+
+					database.apply {
+						name = "org.jooq.meta.postgres.PostgresDatabase"
+						inputSchema = "public"
+					}
+					generate.apply {
+						isDeprecated = false
+						isRecords = true
+						isImmutablePojos = true
+						isFluentSetters = true
+					}
+					target.apply {
+						packageName = "com.tr.domain.generated"
+					}
+					strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+				}
+			}
+		}
+	}
 }
